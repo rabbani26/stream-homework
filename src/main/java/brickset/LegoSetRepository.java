7bac8c502c539d945329cc838161633c6a83af40
Omar Ijaz Rabbani <omarijaz.26@hotmail.com>
2022-03-23 23:56:07 +0100

    Omar Ijaz Rabbani Add new methods

--- src/main/java/brickset/LegoSetRepository.java
+++ src/main/java/brickset/LegoSetRepository.java
@@ -2,6 +2,8 @@ package brickset;
 
 import repository.Repository;
 
+import java.util.Comparator;
+
 /**
  * Represents a repository of {@code LegoSet} objects.
  */
@@ -11,4 +13,44 @@ public class LegoSetRepository extends Repository<LegoSet> {
         super(LegoSet.class, "brickset.json");
     }
 
+    public void printLegoSetAlphabeticalOrder() {
+        getAll().stream().
+                map(LegoSet::getName).
+                sorted(Comparator.nullsFirst(Comparator.naturalOrder())).
+                forEach(System.out::println);
+    }
+
+    public long maximumPieces(){
+        return getAll().stream().
+                mapToLong(LegoSet::getPieces).
+                max().
+                getAsLong();
+    }
+
+    public void printNameOfThemeGame(){
+        getAll().stream().
+                filter(legoset -> legoset.getTheme().equals("Games")).
+                map(LegoSet::getName).
+                forEach(System.out::println);
+    }
+    public void printNamePiecesLessThanHundred(){
+        getAll().stream().
+                filter(legoset -> legoset.getPieces() < 100).
+                forEach(System.out::println);
+    }
+
+    public long sumBricksThemeDuplo(){
+        return getAll().stream().
+                filter(legoset -> legoset.getTheme().equals("Duplo")).
+                mapToLong(LegoSet::getPieces).
+                sum();
+    }
+    public static void main(String[] args) {
+        var legos = new LegoSetRepository();
+        legos.printLegoSetAlphabeticalOrder();
+        System.out.println(legos.maximumPieces());
+        legos.printNameOfThemeGame();
+        legos.printNamePiecesLessThanHundred();
+        System.out.println(legos.sumBricksThemeDuplo());
+    }
 }
