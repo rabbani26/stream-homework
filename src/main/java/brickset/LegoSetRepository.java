dfb98086097ed9386811708b7f71562a5297f674
Omar Ijaz Rabbani <omarijaz.26@hotmail.com>
2022-04-14 23:19:18 +0200

    Second homework of streams updated Omar Ijaz Rabbani omarijaz.26@hotmail.com

--- src/main/java/brickset/LegoSetRepository.java
+++ src/main/java/brickset/LegoSetRepository.java
@@ -3,6 +3,9 @@ package brickset;
 import repository.Repository;
 
 import java.util.Comparator;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * Represents a repository of {@code LegoSet} objects.
@@ -45,6 +48,41 @@ public class LegoSetRepository extends Repository<LegoSet> {
                 mapToLong(LegoSet::getPieces).
                 sum();
     }
+
+
+    //SECOND STREAMS HOMEWORK
+    /**
+     * Returns boolean value for whether
+     * there is a "Star Wars" theme present.
+     */
+    public boolean legoSetsStarWarsTheme(){
+        return getAll().stream().
+                anyMatch(legoset -> legoset.getTheme().equals("Star Wars"));
+    }
+
+    //public long countDistinctThemes(){
+     //   return getAll().stream()
+      //          .flatMap(legoset -> legoset.getTheme().stream())
+        //        .collect(groupingBy(Function.identity(), counting()));
+    //}
+
+    /**
+     * returns integer value of sum of legoset pieces
+     */
+    public int sumLegoSetPieces(){
+        return getAll().stream().
+                map(LegoSet::getPieces).
+                reduce(0, (a, b) -> a + b);
+
+    }
+
+    /**
+     *
+     * @param
+     */
+
+
+
     public static void main(String[] args) {
         var legos = new LegoSetRepository();
         legos.printLegoSetAlphabeticalOrder();
@@ -52,5 +90,7 @@ public class LegoSetRepository extends Repository<LegoSet> {
         legos.printNameOfThemeGame();
         legos.printNamePiecesLessThanHundred();
         System.out.println(legos.sumBricksThemeDuplo());
+        System.out.println(legos.legoSetsStarWarsTheme());
+        System.out.println((legos.sumLegoSetPieces()));
     }
 }
